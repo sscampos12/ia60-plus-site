e5e540e890bfac435cd35de1a5148f60023721d1
Suzy Soares Campos <suzyscampos@hotmail.com>
2025-06-25 04:00:27 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// import path from 'path' // Remova ou comente esta linha
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  root: './', // Adicione esta linha
   base: './',
-  plugins: [react( ),tailwindcss()],
-  // Remova todo o bloco 'resolve' abaixo
-  // resolve: {
-  //   alias: {
-  //     "@": path.resolve(__dirname, "./src"),
-  //   },
-  // },
+  plugins: [react(),tailwindcss()],
 })
+
